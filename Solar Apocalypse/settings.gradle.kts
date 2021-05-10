2a17e291a13ae57ec210dfd63695b7df4543c85f
SmushyTaco <smushytaco@gmail.com>
2021-05-09 19:20:15 -0700

    Initial Commit

---
+++ Solar Apocalypse/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven("https://maven.fabricmc.net") { name = "Fabric" }
+        gradlePluginPortal()
+    }
+    plugins {
+        val loomVersion: String by settings
+        id("fabric-loom").version(loomVersion)
+        val kotlinVersion: String by System.getProperties()
+        kotlin("jvm").version(kotlinVersion)
+    }
+}
---
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/LeavesBlockDecayInDaylight.java
@@ -0,0 +1,24 @@
+package com.smushytaco.solar_apocalypse.mixins;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Random;
+@Mixin(LeavesBlock.class)
+public abstract class LeavesBlockDecayInDaylight {
+    @Inject(method = "hasRandomTicks", at = @At("HEAD"), cancellable = true)
+    private void hookHasRandomTicks(BlockState state, CallbackInfoReturnable<Boolean> cir) { cir.setReturnValue(true); }
+    @Inject(method = "randomTick", at = @At("HEAD"))
+    private void hookRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        BlockPos blockPos = pos.offset(Direction.UP);
+        double worldAge = world.getTimeOfDay() / 24000.0D;
+        if (worldAge < 5.0D || world.isNight() || world.isRaining() || !world.isSkyVisible(blockPos) || !state.getMaterial().isBurnable()) return;
+        world.removeBlock(pos, false);
+    }
+}
