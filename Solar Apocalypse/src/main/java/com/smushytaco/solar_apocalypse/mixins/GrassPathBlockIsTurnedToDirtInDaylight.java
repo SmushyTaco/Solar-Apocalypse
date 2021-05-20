cc5ee4ef79273a68fd09f4f95af74e35e8976f6d
Nikan Radan <github.com@alias.nikanradan.com>
2021-05-20 10:45:07 -0700

    Grass Path Bug Fix

---
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/GrassPathBlockIsTurnedToDirtInDaylight.java
@@ -0,0 +1,24 @@
+package com.smushytaco.solar_apocalypse.mixins;
+import com.smushytaco.solar_apocalypse.SolarApocalypse;
+import com.smushytaco.solar_apocalypse.WorldDayCalculation;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.GrassPathBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import java.util.Random;
+@SuppressWarnings("deprecation")
+@Mixin(GrassPathBlock.class)
+public abstract class GrassPathBlockIsTurnedToDirtInDaylight extends Block {
+    public GrassPathBlockIsTurnedToDirtInDaylight(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        BlockPos blockPos = pos.offset(Direction.UP);
+        if (!WorldDayCalculation.isOldEnough(world, SolarApocalypse.INSTANCE.getConfig().getBlocksAndWaterAreAffectedByDaylightDay()) || world.isNight() || world.isRaining() || !world.isSkyVisible(blockPos)) return;
+        super.randomTick(state, world, pos, random);
+    }
+}
