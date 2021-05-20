03155ab21bd2fb64bedf7326d0ec6727c57febf2
Nikan Radan <github.com@alias.nikanradan.com>
2021-05-20 02:12:17 -0700

    Update 1.2.0

--- Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/CertainBlocksHaveRandomTick.java
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/CertainBlocksHaveRandomTick.java
@@ -1,15 +1,13 @@
 package com.smushytaco.solar_apocalypse.mixins;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Block.class)
-public abstract class ClayHasRandomTick {
+public abstract class CertainBlocksHaveRandomTick {
     @Inject(method = "hasRandomTicks", at = @At("HEAD"), cancellable = true)
     private void hookHasRandomTicks(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-        if (state.getBlock() == Blocks.CLAY) cir.setReturnValue(true);
+        if (state.getBlock() == Blocks.CLAY || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.GRASS_PATH || state.getBlock() instanceof GourdBlock || state.getBlock() instanceof CarvedPumpkinBlock) cir.setReturnValue(true);
     }
 }
---
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/CropsAreDestroyedInDaylight.java
@@ -0,0 +1,29 @@
+package com.smushytaco.solar_apocalypse.mixins;
+import com.smushytaco.solar_apocalypse.SolarApocalypse;
+import com.smushytaco.solar_apocalypse.WorldDayCalculation;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CropBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.Random;
+@Mixin(CropBlock.class)
+public abstract class CropsAreDestroyedInDaylight {
+    @Inject(method = "hasRandomTicks", at = @At("HEAD"), cancellable = true)
+    private void hookHasRandomTick(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+    @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
+    private void hookRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+        BlockPos blockPos = pos.offset(Direction.UP);
+        if (!WorldDayCalculation.isOldEnough(world, SolarApocalypse.INSTANCE.getConfig().getBlocksAndWaterAreAffectedByDaylightDay()) || world.isNight() || world.isRaining() || !world.isSkyVisible(blockPos)) return;
+        world.setBlockState(pos, Blocks.AIR.getDefaultState());
+        ci.cancel();
+    }
+}
