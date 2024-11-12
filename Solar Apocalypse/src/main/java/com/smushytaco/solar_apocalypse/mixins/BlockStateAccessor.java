7785051fe96de85fbad3ca4c9ac696a319d438c3
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-12 03:18:16 -0800

    Touch Ups

--- Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/BlockStateAccessor.java
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/BlockStateAccessor.java
@@ -1,13 +1,9 @@
 package com.smushytaco.solar_apocalypse.mixins;
 import net.minecraft.block.AbstractBlock;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(AbstractBlock.AbstractBlockState.class)
 public interface BlockStateAccessor {
-    @Mutable
-    @Final
     @Accessor
-    void setBurnable(boolean burnable);
+    boolean getBurnable();
 }
