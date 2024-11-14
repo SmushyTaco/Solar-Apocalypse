b4085b048976a8ca69c0a0e729d935a238f0e1d4
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-14 00:37:19 -0800

    Improvements

--- Solar Apocalypse/src/main/kotlin/com/smushytaco/solar_apocalypse/mixin_logic/HeatOverlayFadeInAndOutLogic.kt
+++ Solar Apocalypse/src/main/kotlin/com/smushytaco/solar_apocalypse/mixin_logic/HeatOverlayFadeInAndOutLogic.kt
@@ -10,10 +10,11 @@ import kotlin.math.ceil
 object HeatOverlayFadeInAndOutLogic {
     fun ClientPlayerEntity.hookTick(value: Float, time: Double, day: Double): Float {
         val totalTicks = ceil(time * 20).toInt()
-        val should = world.isOldEnough(day) && isAlive && !world.isRaining && !isSpectator && !isCreative && !world.isNight && (world.isSkyVisible(blockPos) || shouldHeatLayerDamage(world)) && !hasStatusEffect(sunscreen)
+        val should = transitionConditions(day)
         if (totalTicks <= 0) return if (should) 1.0F else 0.0F
         return if (should) (value + (1.0F / totalTicks)).coerceIn(0.0F, 1.0F) else (value - (1.0F / totalTicks)).coerceIn(0.0F, 1.0F)
     }
+    fun ClientPlayerEntity.transitionConditions(day: Double) = world.isOldEnough(day) && isAlive && !world.isRaining && !isSpectator && !isCreative && !world.isNight && (world.isSkyVisible(blockPos) || shouldHeatLayerDamage(world)) && !hasStatusEffect(sunscreen)
     fun hookTick(value: Float, time: Double): Float {
         val totalTicks = ceil(time * 20).toInt()
         if (totalTicks <= 0) return 1.0F
