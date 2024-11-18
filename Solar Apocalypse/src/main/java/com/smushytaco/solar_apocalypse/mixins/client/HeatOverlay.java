49ba3a63ee998c0c603103e2bef0490aaf51d177
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-17 20:08:22 -0800

    Code Clean Up

--- Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/client/HeatOverlay.java
+++ Solar Apocalypse/src/main/java/com/smushytaco/solar_apocalypse/mixins/client/HeatOverlay.java
@@ -1,12 +1,10 @@
 package com.smushytaco.solar_apocalypse.mixins.client;
 import com.smushytaco.solar_apocalypse.SolarApocalypse;
 import com.smushytaco.solar_apocalypse.SolarApocalypseClient;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,9 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class HeatOverlay {
     @Shadow
     protected abstract void renderOverlay(DrawContext context, Identifier texture, float opacity);
-    @Shadow
-    @Final
-    private MinecraftClient client;
     @Inject(method = "renderMiscOverlays", at = @At("RETURN"))
     private void hookRenderMiscOverlays(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         if (!SolarApocalypse.INSTANCE.getConfig().getEnableHeatOverlay() || SolarApocalypseClient.INSTANCE.getOverlayOpacity() == 0.0F) return;
